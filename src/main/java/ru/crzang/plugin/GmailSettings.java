9491f991f609c3955d220c8f5ec427a41295145a
crzang <va7entin@gmail.com>
2014-10-15 00:03:36 +0700

    setup gradle

---
+++ src/main/java/ru/crzang/plugin/GmailSettings.java
@@ -0,0 +1,16 @@
+package ru.crzang.plugin;
+
+/**
+ * @author by crzang.
+ */
+public class GmailSettings implements Settings {
+  @Override
+  public String getLogin() {
+    return "va7entin";
+  }
+
+  @Override
+  public String getPassword() {
+    return "xlludosmwrvqtfzx";
+  }
+}
